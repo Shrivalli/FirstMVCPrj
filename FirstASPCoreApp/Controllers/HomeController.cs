2339c0c8c77ad855b23a4eee74b7ebbcfa7862e1
Shrivalli Maheshwaran <shrivalli04@gmail.com>
2020-09-26 20:56:03 +0530

    Message method modified

--- FirstASPCoreApp/Controllers/HomeController.cs
+++ FirstASPCoreApp/Controllers/HomeController.cs
@@ -32,7 +32,7 @@ namespace FirstASPCoreApp.Controllers
         //Comment Added
         public string msg()
         {
-            return "msg";
+            return "Shrivalli";
         }
         public string welcome()
         {
