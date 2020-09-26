32fb9292f6a26c5eb7e5f031cd713fdf2522d5e6
Shrivalli Maheshwaran <shrivalli04@gmail.com>
2020-09-26 20:18:45 +0530

    First commit

---
+++ FirstASPCoreApp/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FirstASPCoreApp.Models;
+
+namespace FirstASPCoreApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        IList<string> fruits = new List<string>();
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult ShowFruits()
+        {
+            fruits.Add("Apple");
+            fruits.Add("Lemon");
+            fruits.Add("Banana");
+            fruits.Add("Kiwi");
+            ViewBag.Fr = fruits;
+            return View();
+        }
+    }
+}
